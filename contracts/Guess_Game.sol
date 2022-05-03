a93fc50c3079ea0c56185c0ffec11fd7497675b1
Simon <salviensky@gmail.com>
2022-05-03 14:26:40 +0100

    "Done with the Guess Game"

--- contracts/Guess_Game.sol
+++ contracts/Guess_Game.sol
@@ -3,23 +3,29 @@ pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
 
+// Check out the official document to learn more about every concept here: https://docs.soliditylang.org/en/v0.8.13/index.html
 contract Guess_Game {
-    // Declares a state variable `random_number` of type `uint4` which is just an unsigned 4 bit integer.
-    // State variables are variables whose values are permanently stored in contract storage.
+
+    //  Declared state variables whose values are permanently stored in contract storage.
     uint random_number;
     bool game_completion;
+    address winner;
 
     // This is an Struct that defines value for the user that interacting with the smart contract
     struct player {
-        address player_address;
+        uint guess;
         uint trials;
+        bool registered;
     }
 
-    // We instantiated a array of player structs that keeps tracks of every user that has even used the dapp
-    player[] public players;
+    // Created a mapping of an address to the player Struct
+    mapping (address => player) player_structs;
 
-    // Similar to many class-based object-oriented languages, a constructor is a special function that is only executed upon contract creation.
-    // Constructors are used to initialize the contract's data. Learn more:https://solidity.readthedocs.io/en/v0.5.10/contracts.html#constructors
+    // Created a dynamic array of player addresses 
+    address[] internal players_addresses;
+
+
+    // Defining the Contructor which is a block of code that runs the instant the contract is deployed
     constructor() {
 
         // Depending on the timestamp of the current block, the keccak function will generate a random number and we're keeping it between 1 and 100
@@ -29,44 +35,63 @@ contract Guess_Game {
         game_completion = false;
     }
 
-    // Updates the players array with the user details when he tries to
-    // msg.sender is a way of retrieving the users address and we're assigning 10 trials to the user 
-    function Add_Player() public {
-        player memory new_player = player(msg.sender, 10);
-        players.push(new_player);
+    // Generates a new struct with msg.sender which just the user's wallet address and appends it to the array 
+    function set_Player() internal {
+        player_structs[msg.sender].guess = 0;
+        player_structs[msg.sender].trials = 5;
+        player_structs[msg.sender].registered = true;
+
+        players_addresses.push(msg.sender);
     }
 
-    function Guess(uint) public {
-        uint trials_remaining;
-        bool registered;
+    // A function for verifying if a user has been tried to guess before and is registered
+    function get_Player(address _player) internal view returns(bool) {
+        return player_structs[_player].registered;
+    }
 
-        if (players.length == 0) {
-            Add_Player();
+    // This function is visible to the user, it reads the user's input and calls the game function
+    function Guess(uint _Guess) public {
+        if (game_completion) {
+            console.log("Game has been completed and the winning address is ", winner);
+        }
+
+        // Checks to see if player has been assigned a struct
+        if (get_Player(msg.sender) != false) {
+            set_Player();
+        }
+        
+        // Passes in the input and stores it for that particular player
+        player_structs[msg.sender].guess = _Guess;
+
+        Game();
+    }
+
+    function Game() internal {
+        uint guess = player_structs[msg.sender].guess;
+        uint attempts = player_structs[msg.sender].trials;
+
+        if (attempts > 0) {
+            if (guess > random_number) {
+                console.log("Your guess was too big, Guess again");
+            }
+
+            else if (guess < random_number) {
+                console.log("Your guess was too small, Guess again");
+            }
+            
+            else if (guess == random_number && game_completion) {
+                console.log("You got the number but the game has already been won");
+            }
+
+            else if (guess == random_number && !game_completion) {
+                console.log("Congrats! You won the guess game first");
+                winner = msg.sender;
+                game_completion = true;
+            }
         }
 
         else {
-            for(uint i = 0; i < players.length; i++) {
-                if (players[i].player_address == msg.sender) {
-                    trials_remaining = players[i].trials;
-                    registered = true;
-                    break;
-                }
-            }
-
-            if (registered = false) {
-                Add_Player();
-            }
+            console.log("You're out of attempts");
         }
-        
-        require(game_completion, "The game has already been won");
-
-        for (uint i = 0; i < trials_remaining; trials_remaining--) {
-
-        }
-
-
-
-
-
     }
 }
